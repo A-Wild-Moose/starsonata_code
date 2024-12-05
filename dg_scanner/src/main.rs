ec2b0bbccb297c39eaa66ad76bca5e3404d0d9d3
A-Wild-Moose <swolf950@gmail.com>
2024-12-04 22:32:51 -0500

    parsing of galaxy name and ship names within dg galaxies

--- dg_scanner/src/main.rs
+++ dg_scanner/src/main.rs
@@ -1,8 +1,37 @@
 use std::sync::{Arc, atomic::{AtomicBool, Ordering}};
 use pcap::{Device, Capture};
+use regex::{Regex, Captures, CaptureMatches};
+use once_cell::sync::Lazy;
 
 use std::fs::File;
 use std::io::Write;
+
+fn parse_dg(a: &str) -> String {
+    static RE_DG: Lazy<Regex> = Lazy::new(|| Regex::new(
+        r"DG (?<dg_gal>[[:word:] ]*) (?<dg_level>[0-9]{1,2}\.[0-9]+[A-Z])"
+    ).unwrap());
+    let caps = RE_DG.captures(a).unwrap();
+
+    let dg_gal = caps.name("dg_gal").unwrap().as_str();
+    let dg_level = caps.name("dg_level").unwrap().as_str();
+
+    println!("{} {}", dg_gal, dg_level);
+
+    format!("{} {}", dg_gal, dg_level)
+}
+
+fn parse_ships(a: &str) {
+    static RE_SHIPS: Lazy<Regex> = Lazy::new(|| Regex::new(
+        r"(?<ship>[[:word:] ]*)\u{0000}(Light Fighter|Heavy Fighter|Support Freighter|Capital Ship|Organic)"
+        // r"(?<ship>[[:word:] ]{5,11})"
+    ).unwrap());
+    
+    let caps_ship = RE_SHIPS.captures_iter(a);
+    for cap in caps_ship {
+        let (_, [ship, ship_type]) = cap.extract();
+        println!("\t{}", ship.to_string());
+    }
+}
 
 fn main() {
     let running = Arc::new(AtomicBool::new(true));
@@ -29,11 +58,22 @@ fn main() {
         "src host 51.222.248.34", true
     );
 
-    let mut f = File::create("packet_dump.log").expect("unable to create file");
+    // let mut f = File::create("packet_dump.log").expect("unable to create file");
+
+    let mut curr_dg: String = "".to_string();
 
     while running.load(Ordering::SeqCst) {
         let packet = cap.next_packet().unwrap();
         let data = String::from_utf8_lossy(packet.data);
-        f.write_all(data.as_bytes()).expect("unable to write to file");
+        // f.write_all(data.as_bytes()).expect("unable to write to file");
+        if data.contains(std::str::from_utf8(&vec![5, 68, 71, 32]).unwrap()) { // ENQ/DG /
+            curr_dg = parse_dg(&data);
+        }
+        if curr_dg != "" {
+            parse_ships(&data);
+        }
+        if data.contains(format!("Entering DG {}", curr_dg).as_str()) {
+            curr_dg = "".to_string();
+        }
     }
 }
---
+++ dg_scanner/target/debug/build/pcap-5fd079754f3c55a6/build_script_build-5fd079754f3c55a6.d
@@ -0,0 +1,5 @@
+C:\Users\lukas\Documents\starsonata_code\dg_scanner\target\debug\build\pcap-5fd079754f3c55a6\build_script_build-5fd079754f3c55a6.exe: C:\Users\lukas\.cargo\registry\src\index.crates.io-6f17d22bba15001f\pcap-2.2.0\build.rs
+
+C:\Users\lukas\Documents\starsonata_code\dg_scanner\target\debug\build\pcap-5fd079754f3c55a6\build_script_build-5fd079754f3c55a6.d: C:\Users\lukas\.cargo\registry\src\index.crates.io-6f17d22bba15001f\pcap-2.2.0\build.rs
+
+C:\Users\lukas\.cargo\registry\src\index.crates.io-6f17d22bba15001f\pcap-2.2.0\build.rs:
